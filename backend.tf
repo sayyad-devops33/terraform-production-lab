2a8546736167a127ff35eda77b63776732bc55d2
mansoor <sayyadmansoor@gmail.com>
2026-07-15 13:01:07 +0000

    Added S3 remote backend configuration

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "mansoor-terraform-state-991524242348"
+    key            = "production/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
